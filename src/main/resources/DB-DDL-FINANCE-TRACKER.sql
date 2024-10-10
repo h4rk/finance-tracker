dbc50264e7b7783be19c8fac4bffa3c07e680c03
Alessandro Sala <ale.sala@reply.it>
2024-10-10 12:38:19 +0200

    Update DB creation script

--- src/main/resources/DB-DDL-FINANCE-TRACKER.sql
+++ src/main/resources/DB-DDL-FINANCE-TRACKER.sql
@@ -1,9 +1,22 @@
+DROP TABLE IF EXISTS `finance-tracker`.`mov_cat`;
+DROP TABLE IF EXISTS `finance-tracker`.`mov`;
+DROP TABLE IF EXISTS `finance-tracker`.`cat`;
+DROP TABLE IF EXISTS `finance-tracker`.`cat_type`;
+
+CREATE TABLE `finance-tracker`.`cat_type` (
+  `cat_type_id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`cat_type_id`),
+  UNIQUE KEY `unique_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `finance-tracker`.`cat` (
   `cat_id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `description` varchar(150) DEFAULT NULL,
-  `cat_type` tinyint NOT NULL,
-  PRIMARY KEY (`cat_id`)
+  `cat_type` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`cat_id`),
+  CONSTRAINT `fk_cat_type_id` FOREIGN KEY (`cat_type`) REFERENCES `cat_type` (`cat_type_id`) ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `finance-tracker`.`mov` (
@@ -23,3 +36,5 @@ CREATE TABLE `finance-tracker`.`mov_cat` (
   CONSTRAINT `fk_cat_id` FOREIGN KEY (`cat_id`) REFERENCES `cat` (`cat_id`) ON DELETE CASCADE,
   CONSTRAINT `fk_mov_id` FOREIGN KEY (`mov_id`) REFERENCES `mov` (`mov_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table representing N to M relationship between mov table and cat table';
+
+INSERT INTO `finance-tracker`.`cat_type` (`name`) VALUES ('Income'), ('Expense'), ('Both');

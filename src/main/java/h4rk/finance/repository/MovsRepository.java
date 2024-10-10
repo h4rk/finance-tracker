fd288f0044011ef7346346ad36e32c8630ee5462
Alessandro Sala <ale.sala@reply.it>
2024-10-10 12:09:55 +0200

    Fix queries for mov table

--- src/main/java/h4rk/finance/repository/MovsRepository.java
+++ src/main/java/h4rk/finance/repository/MovsRepository.java
@@ -18,7 +18,7 @@ public class MovsRepository {
 	}
 	
 	public int deleteMovs(long id) {
-		return jdbcTemplate.update("DELETE FROM mov WHERE id = ?", id);
+		return jdbcTemplate.update("DELETE FROM mov WHERE mov_id = ?", id);
 	}
 
 	public int postMovs(Mov mov) {
@@ -26,10 +26,10 @@ public class MovsRepository {
 	}
 
 	public List<Mov> getMovs() {
-		return jdbcTemplate.query("SELECT * FROM mov", (rs, rowNum) -> new Mov(rs.getLong("id"), rs.getString("description"), rs.getDouble("amount"), rs.getDate("date"), rs.getBoolean("isIncome")));
+		return jdbcTemplate.query("SELECT * FROM mov", (rs, rowNum) -> new Mov(rs.getLong("mov_id"), rs.getString("description"), rs.getDouble("amount"), rs.getDate("date"), rs.getBoolean("isIncome")));
 	}
 
 	public Mov getMovById(long id) {
-		return jdbcTemplate.queryForObject("SELECT * FROM mov WHERE id = ?", (rs, rowNum) -> new Mov(rs.getLong("id"), rs.getString("description"), rs.getDouble("amount"), rs.getDate("date"), rs.getBoolean("isIncome")), id);
+		return jdbcTemplate.queryForObject("SELECT * FROM mov WHERE mov_id = ?", (rs, rowNum) -> new Mov(rs.getLong("mov_id"), rs.getString("description"), rs.getDouble("amount"), rs.getDate("date"), rs.getBoolean("isIncome")), id);
 	}
 }

702014c9065ede7010dcb826c521af040b7ba351
DahNova <claudio.novaglio@gmail.com>
2024-10-19 12:29:06 +0200

    Swagger endpoints updated

--- src/main/java/h4rk/finance/controller/IndexController.java
+++ src/main/java/h4rk/finance/controller/IndexController.java
@@ -2,10 +2,14 @@ package h4rk.finance.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Controller
+@Tag(name = "Index", description = "Main page controller")
 public class IndexController {
 
+	@Operation(summary = "Get main page", description = "Retrieves the main page of the application")
 	@GetMapping("/")
 	public String index() {
 		return "index";
--- src/main/java/h4rk/finance/controller/SecurityController.java
+++ src/main/java/h4rk/finance/controller/SecurityController.java
@@ -1,26 +1,29 @@
 package h4rk.finance.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import h4rk.finance.security.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Tag(name = "Security", description = "Security management APIs")
 @Slf4j
 public class SecurityController {
 	
 	@Autowired
 	private UserService userService;
 	
+	@Operation(summary = "Register a new user", description = "Registers a new user with the provided username and password")
 	@PostMapping("/register")
 	@ResponseBody
 	public ResponseEntity<String> register(@RequestParam String username, @RequestParam String password) {
@@ -28,12 +31,14 @@ public class SecurityController {
 		return ResponseEntity.ok("User registered successfully");
 	}
 
+	@Operation(summary = "Get login page", description = "Retrieves the login page")
 	@GetMapping("/login")
 	public String login() {
 		log.info("Login page requested");
 		return "login";
 	}
 
+	@Operation(summary = "Get registration page", description = "Retrieves the registration page")
 	@GetMapping("/register")
 	public String register() {
 		log.info("Register page requested");

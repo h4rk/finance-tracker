f662cc3f95ec37431f74d3a6c459ef6dd515ab61
DahNova <claudio.novaglio@gmail.com>
2024-10-20 11:18:15 +0200

    mockup controller to handle mockup pages

---
+++ src/main/java/h4rk/finance/controller/MockupController.java
@@ -0,0 +1,39 @@
+package h4rk.finance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MockupController {
+
+    @GetMapping("/budget-planner")
+    public String budgetPlanner(Model model) {
+        model.addAttribute("pageTitle", "Budget Planner");
+        return "mockup/budgetplanner";
+    }
+
+    @GetMapping("/analytics")
+    public String analytics(Model model) {
+        model.addAttribute("pageTitle", "Analytics");
+        return "mockup/analytics";
+    }
+
+    @GetMapping("/category-management")
+    public String categoryManagement(Model model) {
+        model.addAttribute("pageTitle", "Category Management");
+        return "mockup/category-management";
+    }
+
+    @GetMapping("/movement-history")
+    public String movementHistory(Model model) {
+        model.addAttribute("pageTitle", "Movement History");
+        return "mockup/movement-history";
+    }
+
+    @GetMapping("/user-profile")
+    public String userProfile(Model model) {
+        model.addAttribute("pageTitle", "User Profile");
+        return "mockup/user-profile";
+    }
+}

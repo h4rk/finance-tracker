2541b3c08a3f59b8203a2397efa1d784b9303a5a
Alessandro Sala <ale.sala@reply.it>
2024-10-23 12:25:44 +0200

    Modificato controller index per stampare utente loggato

--- src/main/java/h4rk/finance/controller/IndexController.java
+++ src/main/java/h4rk/finance/controller/IndexController.java
@@ -1,17 +1,24 @@
 package h4rk.finance.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import h4rk.finance.security.dto.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 @Tag(name = "Index", description = "Main page controller")
 public class IndexController {
 
 	@Operation(summary = "Get main page", description = "Retrieves the main page of the application")
 	@GetMapping("/")
 	public String index() {
+		User u = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		log.info("User: " + u.toString());
 		return "index";
 	}
 	

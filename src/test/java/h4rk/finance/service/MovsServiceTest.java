2584d93ae4ce46ab2510298edeb4d89cac387d1c
Alessandro Sala <ale.sala@reply.it>
2024-10-31 12:20:14 +0100

    New tests

--- src/test/java/h4rk/finance/service/MovsServiceTest.java
+++ src/test/java/h4rk/finance/service/MovsServiceTest.java
@@ -82,23 +82,36 @@ public class MovsServiceTest {
 		assertThrows(PostMovException.class, () -> movsService.postMovs(movWithCat));
     }
 
-	//TODO fix tests
-    // @Test
-    // public void testPutMov() {
-    //     MovWithCat movWithCat = new MovWithCat();
-    //     when(movRepository.save(any(MovWithCat.class))).thenReturn(movWithCat);
+    @Test
+    public void testPutMov() {
+        MovWithCat old = new MovWithCat(1L, "a", 1.0, null, true, Arrays.asList(1L, 2L));
+		MovWithCat movWithCat = new MovWithCat(null, "b", 2.0, null, false, Arrays.asList(1L, 2L));
+		MovWithCat expected = new MovWithCat(1L, "b", 2.0, null, false, Arrays.asList(1L, 2L));
+		
+		List<Cat> catsOk = Arrays.asList(new Cat(1L,"a", "a", (short)1), new Cat(2L,"b", "b", (short)2 ));
+		List<Cat> catsKo = Arrays.asList(new Cat(3L,"a", "a", (short)1), new Cat(2L,"b", "b", (short)2 ));
+		
+		when(catsService.getCats()).thenReturn(catsOk);
+		when(userService.getCurrentUserId()).thenReturn(1L);
+		when(movRepository.getMovWithCatById(anyLong(), anyLong())).thenReturn(old);
+        when(movRepository.putMovs(anyLong(), any(Mov.class), anyLong())).thenReturn(1);
+		movWithCat.setId(1L);
+		when(movRepository.getMovWithCatById(anyLong(), anyLong())).thenReturn(movWithCat);
+        MovWithCat result = movsService.putMovs(1L, movWithCat);
 
-    //     MovWithCat result = movsService.putMov(1L, movWithCat);
+        assertEquals(expected, result);
 
-    //     assertEquals(movWithCat, result);
-    // }
+		when(catsService.getCats()).thenReturn(catsKo);
+		assertThrows(PostMovException.class, () -> movsService.putMovs(1L, movWithCat));
+    }
 
-    // @Test
-    // public void testDeleteMov() {
-    //     doNothing().when(movRepository).deleteById(anyLong());
+    @Test
+    public void testDeleteMov() {
+        doNothing().when(movRepository.deleteMovs(anyLong(), anyLong()));
+		when(userService.getCurrentUserId()).thenReturn(1L);
 
-    //     movsService.deleteMov(1L);
+        movsService.deleteMovs(1L);
 
-    //     verify(movRepository, times(1)).deleteById(1L);
-    // }
+        verify(movRepository, times(1)).deleteMovs(1L, 1L);
+    }
 }

09c22f6a1fdf75607b6fce327edd1f2de0cc1baa
Alessandro Sala <ale.sala@reply.it>
2024-12-02 18:04:50 +0100

    add basic auth support for postman api testing

--- src/main/java/h4rk/finance/configuration/SecurityConfig.java
+++ src/main/java/h4rk/finance/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -48,8 +49,9 @@ public class SecurityConfig {
 			.clearAuthentication(true)
 			.deleteCookies("JSESSIONID")
 			.permitAll())
-      .csrf((csrf) -> csrf.ignoringRequestMatchers("/**"))
-      .build();
+		.csrf((csrf) -> csrf.ignoringRequestMatchers("/**"))
+		.httpBasic(Customizer.withDefaults())
+		.build();
     }
 
 	@Bean
